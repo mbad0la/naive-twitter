04a5350655b14f89b2a8d282599808c548a92e2f
Mayank Badola <badola21295@gmail.com>
2017-05-04 20:47:15 +0530

    FEAT : add http request log middleware

--- server.js
+++ server.js
@@ -6,8 +6,17 @@ const express = require('express')
 const app = express()
 const serverConfig = JSON.parse(fs.readFileSync('serverConfig.json').toString())
 
+app.all('*', (req, res, next) => {
+  log.http(req.method, req.url)
+  next()
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Get World')
+})
+
+app.post('/', (req, res) => {
+  res.send('Post World')
 })
 
 app.listen(serverConfig.port, () => {
