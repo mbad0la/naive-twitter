168ed6c1de4262a39231cb922f4a816b107f0091
Mayank Badola <badola21295@gmail.com>
2020-03-01 10:47:32 +0530

    REFACTOR : App and Auth are now functional components

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
@@ -8,84 +8,62 @@ import ViewWrapper from './View'
 
 import styles from '../app.css'
 
-class App extends Component {
+function App(props) {
+  const { authState, modifyAuthState } = props;
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      onLogin: false,
-      onRegister: false
-    }
-
-    this.press = this.press.bind(this)
-    this.release = this.release.bind(this)
+  if (authState.isAuthenticated) {
+    return (
+      <div className={styles.flex}>
+        <ProfileSearch
+          style={{width: '40%', height: '100%'}}
+          authState={authState}
+          modifyAuthState={modifyAuthState}
+        />
+        <ViewWrapper
+          style={{width: '60%', height: '100%'}}
+          authState={authState}
+          modifyAuthState={modifyAuthState}
+        />
+      </div>
+    )
+  } else {
+    return (
+      <div className={styles.authWrapper}>
+        <ul className={styles.auth_ul}>
+          <li className={styles.auth_li}>
+            <Link to='/login'>
+              <Button
+                variant="contained"
+                color="primary"
+                startIcon={<MeetingRoomIcon />}
+              >
+                Login
+              </Button>
+              {/* <RaisedButton
+                backgroundColor='#ef5b25'
+                icon={<FontIcon style={{fontSize: 22, color: '#ffffff'}} className='fa fa-sign-in'/>}>
+              </RaisedButton> */}
+            </Link>
+          </li>
+          <li className={styles.auth_li}>
+            <Link to='/register'>
+              <Button
+                variant="contained"
+                color="primary"
+                startIcon={<PersonAddIcon />}
+              >
+                Register
+              </Button>
+              {/* <RaisedButton
+                backgroundColor='#ef5b25'
+                icon={<FontIcon style={{fontSize: 20, color: '#ffffff'}} className='fa fa-user-plus'/>}>
+              </RaisedButton> */}
+            </Link>
+          </li>
+        </ul>
+      </div>
+    )
   }
-
-  press(event) {
-    this.setState({ [event.target.attributes['for'].value]: true })
-  }
-
-  release(event) {
-    this.setState({ [event.target.attributes['for'].value]: false })
-  }
-
-  render() {
-    console.log(this.props.authState.isAuthenticated)
-    if (this.props.authState.isAuthenticated) {
-      return (
-        <div className={styles.flex}>
-          <ProfileSearch
-            style={{width: '40%', height: '100%'}}
-            authState={this.props.authState}
-            modifyAuthState={this.props.modifyAuthState}
-          />
-          <ViewWrapper
-            style={{width: '60%', height: '100%'}}
-            authState={this.props.authState}
-            modifyAuthState={this.props.modifyAuthState}
-          />
-        </div>
-      )
-    } else {
-      return (
-        <div className={styles.authWrapper}>
-          <ul className={styles.auth_ul}>
-            <li className={styles.auth_li}>
-              <Link to='/login'>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  startIcon={<MeetingRoomIcon />}
-                >
-                  Login
-                </Button>
-                {/* <RaisedButton
-                  backgroundColor='#ef5b25'
-                  icon={<FontIcon style={{fontSize: 22, color: '#ffffff'}} className='fa fa-sign-in'/>}>
-                </RaisedButton> */}
-              </Link>
-            </li>
-            <li className={styles.auth_li}>
-              <Link to='/register'>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  startIcon={<PersonAddIcon />}
-                >
-                  Register
-                </Button>
-                {/* <RaisedButton
-                  backgroundColor='#ef5b25'
-                  icon={<FontIcon style={{fontSize: 20, color: '#ffffff'}} className='fa fa-user-plus'/>}>
-                </RaisedButton> */}
-              </Link>
-            </li>
-          </ul>
-        </div>
-      )
-    }
-  }
-
 }
 
 export { App }
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Redirect } from 'react-router-dom'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
@@ -21,273 +21,170 @@ const formStyle = {
   }
 }
 
-class Login extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      username: '',
-      password: ''
-    }
-
-    this.login = this.login.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  login(event) {
-    axios.post('/login', this.state)
-      .then(res => {
-        console.log(res.data)
-        localStorage.setItem('postman_naive_twitter_token', res.data.token)
-        localStorage.setItem('postman_naive_twitter_auth', res.data.isAuthenticated)
-        localStorage.setItem('postman_naive_twitter_user', res.data.user)
-        localStorage.setItem('postman_naive_twitter_followers', res.data.followers)
-        this.props.modifyAuthState(res.data)
-      })
-  }
-
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value })
-  }
-
-  render() {
-    if (this.props.authState.isAuthenticated) {
-      return (
-        <Redirect to={{
-          pathname: '/',
-          state: { from: '/login' }
-        }}/>
-      )
-    }
-
-    return (
-      <div className={styles.form}>
-        <TextField
-          name='username'
-          label='Username'
-          value={this.state.username}
-          onChange={this.handleChange}
-          fullWidth={true}
-        />
-        {/* <TextField
-          name='username'
-          value={this.state.username}
-          onChange={this.handleChange}
-          floatingLabelText='Username'
-          underlineFocusStyle={formStyle.underlineStyle}
-          floatingLabelFocusStyle={formStyle.floatingLabelStyle}
-        /> */}
-        <br />
-        <TextField
-          type='password'
-          name='password'
-          label='Password'
-          value={this.state.password}
-          onChange={this.handleChange}
-          fullWidth={true}
-        />
-        {/* <TextField
-          name='password'
-          value={this.state.password}
-          onChange={this.handleChange}
-          floatingLabelText='Password'
-          type='password'
-          underlineFocusStyle={formStyle.underlineStyle}
-          floatingLabelFocusStyle={formStyle.floatingLabelStyle}
-        /> */}
-        <br />
-        <Button 
-          variant='contained' 
-          color='primary'
-          fullWidth={true}
-          onClick={this.login}
-        >
-          Login
-        </Button>
-        {/* <RaisedButton
-          style={{marginTop: 15}}
-          backgroundColor='#ef5b25'
-          label='LOGIN'
-          labelColor='#ffffff'
-          fullWidth={true}
-          onClick={this.login}
-        /> */}
-      </div>
-    )
-  }
-
+function login(payload, modifyAuthState) {
+  axios.post('/login', payload)
+    .then(res => {
+      console.log(res.data)
+      localStorage.setItem('postman_naive_twitter_token', res.data.token)
+      localStorage.setItem('postman_naive_twitter_auth', res.data.isAuthenticated)
+      localStorage.setItem('postman_naive_twitter_user', res.data.user)
+      localStorage.setItem('postman_naive_twitter_followers', res.data.followers)
+      modifyAuthState(res.data)
+    })
 }
 
-/*<input type='text' name='username' placeholder='Username' value={this.state.username} onChange={this.handleChange} />*/
+function register(event, payload, modifiers) {
+  event.preventDefault()
 
-class Register extends Component {
+  const {
+    setFirstName,
+    setLastName,
+    setPassword,
+    setUsername,
+    setErrFirstName,
+    setErrLastName,
+    setErrPassword,
+    setErrUsername
+  } = modifiers
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      firstName: '',
-      lastName: '',
-      username: '',
-      password: '',
-      errFirstName: false,
-      errLastName: false,
-      errUsername: false,
-      errPassword: false
-    }
+  axios.post('/register', payload)
+    .then(res => {
+      if (res.data.success) {
+        setFirstName('');
+        setLastName('');
+        setPassword('');
+        setUsername('');
+        setErrFirstName(false);
+        setErrLastName(false);
+        setErrPassword(false);
+        setErrUsername(false);
 
-    this.register = this.register.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  register(event) {
-    event.preventDefault()
-    let { firstName, lastName, username, password } = this.state
-    axios.post('/register', { firstName, lastName, username, password })
-      .then(res => {
-        if (res.data.success) {
-          this.setState({
-            firstName: '',
-            lastName: '',
-            username: '',
-            password: '',
-            errFirstName: false,
-            errLastName: false,
-            errUsername: false,
-            errPassword: false
-          })
-
-          alert('Successful registration for ' + res.data.user)
-        } else {
-          let validationFails = {
-            errFirstName: false,
-            errLastName: false,
-            errUsername: false,
-            errPassword: false
+        alert('Successful registration for ' + res.data.user)
+      } else {
+        res.data.errors.forEach(error => {
+          if (error == 'firstName') {
+            setErrFirstName(true);
+          } else if (error == 'lastName') {
+            setErrLastName(true);
+          } else if (error == 'username') {
+            setErrUsername(true);
+          } else {
+            setErrPassword(true);
           }
+        })
+      }
+    })
+}
 
-          res.data.errors.forEach(error => {
-            if (error == 'firstName') {
-              validationFails['errFirstName'] = true
-            } else if (error == 'lastName') {
-              validationFails['errLastName'] = true
-            } else if (error == 'username') {
-              validationFails['errUsername'] = true
-            } else {
-              validationFails['errPassword'] = true
-            }
-          })
 
-          this.setState(validationFails)
-        }
-      })
-  }
+function Login(props) {
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
 
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value })
-  }
+  const { authState, modifyAuthState } = props;
 
-  render() {
-    if (this.props.authState.isAuthenticated) {
-      return (
-        <Redirect to={{
-          pathname: '/',
-          state: { from: '/register' }
-        }}/>
-      )
-    }
-
+  if (authState.isAuthenticated) {
     return (
-      <div className={styles.form}>
-        <TextField
-          name='firstName'
-          label='First Name'
-          value={this.state.firstName}
-          onChange={this.handleChange}
-          fullWidth={true}
-        />
-        {/* <TextField
-          name='firstName'
-          value={this.state.firstName}
-          onChange={this.handleChange}
-          floatingLabelText='First Name'
-          underlineFocusStyle={formStyle.underlineStyle}
-          floatingLabelFocusStyle={formStyle.floatingLabelStyle}
-          errorText={this.state.errFirstName?'Required':null}
-        /> */}
-        <br />
-        <TextField
-          name='lastName'
-          label='Last Name'
-          value={this.state.lastName}
-          onChange={this.handleChange}
-          fullWidth={true}
-        />
-        {/* <TextField
-          name='lastName'
-          value={this.state.lastName}
-          onChange={this.handleChange}
-          floatingLabelText='Last Name'
-          underlineFocusStyle={formStyle.underlineStyle}
-          floatingLabelFocusStyle={formStyle.floatingLabelStyle}
-          errorText={this.state.errLastName?'Required':null}
-        /> */}
-        <br />
-        <TextField
-          name='username'
-          label='Username'
-          value={this.state.username}
-          onChange={this.handleChange}
-          fullWidth={true}
-        />
-        {/* <TextField
-          name='username'
-          value={this.state.username}
-          onChange={this.handleChange}
-          floatingLabelText='Username'
-          underlineFocusStyle={formStyle.underlineStyle}
-          floatingLabelFocusStyle={formStyle.floatingLabelStyle}
-          errorText={this.state.errUsername?'Required or Already taken':null}
-        /> */}
-        <br />
-        <TextField
-          type='password'
-          name='password'
-          label='Password'
-          value={this.state.password}
-          onChange={this.handleChange}
-          fullWidth={true}
-        />
-        {/* <TextField
-          name='password'
-          value={this.state.password}
-          onChange={this.handleChange}
-          floatingLabelText='Password'
-          underlineFocusStyle={formStyle.underlineStyle}
-          floatingLabelFocusStyle={formStyle.floatingLabelStyle}
-          type='password'
-          errorText={this.state.errPassword?'Must be more than 6 characters':null}
-        /> */}
-        <br />
-        <Button 
-          variant='contained' 
-          color='primary'
-          fullWidth={true}
-          onClick={this.register}
-        >
-          Register
-        </Button>
-        {/* <RaisedButton
-          style={{marginTop: 15}}
-          backgroundColor='#ef5b25'
-          label='REGISTER'
-          labelColor='#ffffff'
-          fullWidth={true}
-          onClick={this.register}
-        /> */}
-      </div>
+      <Redirect to={{
+        pathname: '/',
+        state: {from: '/login'}
+      }}/>
     )
   }
 
+  return (
+    <div className={styles.form}>
+      <TextField
+        label='Username'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        fullWidth={true}
+      />
+      <br />
+      <TextField
+        type='password'
+        label='Password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        fullWidth={true}
+      />
+      <br />
+      <Button 
+        variant='contained' 
+        color='primary'
+        fullWidth={true}
+        onClick={() => login({username, password}, modifyAuthState)}
+      >
+        Login
+      </Button>
+    </div>
+  )
+}
+
+function Register(props) {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errFirstName, setErrFirstName] = useState(false)
+  const [errLastName, setErrLastName] = useState(false)
+  const [errUsername, setErrUsername] = useState(false)
+  const [errPassword, setErrPassword] = useState(false)
+
+  
+  const {authState} = props;
+
+  if (authState.isAuthenticated) {
+    return (
+      <Redirect to={{
+        pathname: '/',
+        state: { from: '/register' }
+      }}/>
+    )
+  }
+
+  return (
+    <div className={styles.form}>
+      <TextField
+        label='First Name'
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+        fullWidth={true}
+      />
+      <br />
+      <TextField
+        label='Last Name'
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+        fullWidth={true}
+      />
+      <br />
+      <TextField
+        label='Username'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        fullWidth={true}
+      />
+      <br />
+      <TextField
+        type='password'
+        label='Password'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        fullWidth={true}
+      />
+      <br />
+      <Button 
+        variant='contained' 
+        color='primary'
+        fullWidth={true}
+        onClick={(e) => register(e, {firstName, lastName, username, password}, {setFirstName, setLastName, setUsername, setPassword, setErrFirstName, setErrLastName, setErrPassword, setErrUsername})}
+      >
+        Register
+      </Button>
+    </div>
+  )
 }
 
 export { Login, Register }
